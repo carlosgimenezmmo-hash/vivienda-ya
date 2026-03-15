5eb18def1c921052ccf109e151757db1d52c39ee
Carlos <carlosgimenezmmo@gmail.com>
2026-03-15 18:29:20 -0300

    fix: label htmlFor para avatar upload mobile

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -114,8 +114,8 @@ export default function PerfilPage() {
           }}>
             {user.avatar_url ? <img src={user.avatar_url} style={{ width: "100%", height: "100%", objectFit: "cover" }} /> : initials}
           </div>
-          <div 
-  onClick={() => document.getElementById('avatar-upload')?.click()}
+          <label htmlFor="avatar-upload"
+
   style={{
     position: "absolute", bottom: 0, right: 0,
     width: 24, height: 24, borderRadius: "50%",
@@ -125,9 +125,9 @@ export default function PerfilPage() {
   }}>
   <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2.5" strokeLinecap="round">
     <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
-    <circle cx="12" cy="13" r="4"/>
   </svg>
-</div>
+</label>
+
 <input
   id="avatar-upload"
   type="file"
