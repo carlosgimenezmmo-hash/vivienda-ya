5632a47e0e156daae647d9b5286acb61e43bfa25
Carlos <carlosgimenezmmo@gmail.com>
2026-03-29 18:22:35 -0300

    feat: nuevos iconos PWA ViviendaYa

---
+++ generate-icons.js
@@ -0,0 +1,19 @@
+﻿const sharp = require("sharp");
+const fs = require("fs");
+
+const svg = fs.readFileSync("./public/icon.svg");
+
+sharp(svg).resize(192, 192).png().toFile("./public/icon-192.png", (err) => {
+  if (err) console.error("Error 192:", err);
+  else console.log("icon-192.png creado");
+});
+
+sharp(svg).resize(512, 512).png().toFile("./public/icon-512.png", (err) => {
+  if (err) console.error("Error 512:", err);
+  else console.log("icon-512.png creado");
+});
+
+sharp(svg).resize(180, 180).png().toFile("./public/apple-icon.png", (err) => {
+  if (err) console.error("Error apple:", err);
+  else console.log("apple-icon.png creado");
+});
