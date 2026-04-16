b8a55f19f8a721df88b2eacf230c52638b2362f6
Carlos <carlosgimenezmmo@gmail.com>
2026-04-16 19:30:31 -0300

    fix: supabase client con keys directas

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://issofymslvkyfimdnlqe.supabase.co'
+const supabaseAnonKey = 'sb_publishable_2KTRPUlnFuFC84Ek_NkyfA_TQVn7Pu6'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
