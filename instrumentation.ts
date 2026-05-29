03cfb6a8fbc7262c6be1317abb55d32bee351b05
Carlos <carlosgimenezmmo@gmail.com>
2026-05-29 20:29:25 -0300

    fix: logtail constructor error

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,10 +1,8 @@
-export async function register() {
+﻿export async function register() {
   try {
     if (process.env.BETTERSTACK_TOKEN) {
-      const { Logtail } = await import("@logtail/next")
-      const logtail = new Logtail(process.env.BETTERSTACK_TOKEN)
-      logtail.info("App iniciada")
-      await logtail.flush()
+      const { log } = await import("@logtail/next")
+      log.info("App iniciada")
     }
   } catch (err) {
     console.error("Logtail error:", err)
